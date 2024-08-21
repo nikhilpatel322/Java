99348bc6ea3b478c26137557f2027ec5f31dda05
nikhilpatel322 <nikhilpateludemy.npl@gmail.com>
2024-08-21 20:31:14 +0530

    Add files via upload
    Sum of two numbers

--- App.java
+++ App.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("This is the first java program!");
+    public static void main(String[] args) 
+    {
+        Scanner scanner= new Scanner(System.in);
+    
+
+        System.out.println("enter num1");
+        int num1=scanner.nextInt();
+
+        System.out.println("enter num2");
+        int num2=scanner.nextInt();
+        int sum=num1+num2;
+
+     
+        System.out.println("The sum is " + sum);
     }
 }
