0758afc0b9dc0b9e3b56ac9573bec86350231a71
nikhilpatel322 <nikhilpateludemy.npl@gmail.com>
2024-08-26 15:33:30 +0530

    Add files via upload

---
+++ MethodOverriding.java
@@ -0,0 +1,31 @@
+class Animal {
+    void makeSound() {
+        System.out.println("Some generic animal sound");
+    }
+}
+
+class Dog extends Animal {
+    @Override
+    void makeSound() {
+        System.out.println("Bark");
+    }
+}
+
+class Cat extends Animal {
+    @Override
+    void makeSound() {
+        System.out.println("Meow");
+    }
+}
+
+public class MethodOverriding {
+    public static void main(String[] args) {
+        Animal myAnimal = new Animal();
+        Animal myDog = new Dog();
+        Animal myCat = new Cat();
+
+        myAnimal.makeSound();  // Outputs: Some generic animal sound
+        myDog.makeSound();     // Outputs: Bark
+        myCat.makeSound();     // Outputs: Meow
+    }
+}
