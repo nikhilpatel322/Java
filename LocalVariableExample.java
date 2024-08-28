ca3ffc6fb9b696bc2aea017a01033002fc5f9cbe
nikhilpatel322 <nikhilpateludemy.npl@gmail.com>
2024-08-28 21:06:29 +0530

    Add files via upload

---
+++ LocalVariableExample.java
@@ -0,0 +1,8 @@
+public class LocalVariableExample {
+    public static void main(String[] args) {
+        int x = 5; // Local variable
+        int y = 7; // Local variable
+        int sum = x + y; // Local variable
+        System.out.println("The sum is: " + sum);
+    }
+}
